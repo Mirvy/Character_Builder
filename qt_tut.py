ca15b78faafef1d74c38469e818fc5f3c145e985
Mike <michael.yelverton89@gmail.com>
2022-10-24 19:25:59 -0700

    Added an example of using keypress events.

--- qt_tut.py
+++ qt_tut.py
@@ -1,24 +1,21 @@
 import sys
-
-from PyQt5.QtWidgets import (QApplication, QWidget, QSlider, QLCDNumber, QVBoxLayout)
+from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtCore import Qt
 
 class Example(QWidget):
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-       lcd = QLCDNumber(self)
-       sld = QSlider(Qt.Horizontal, self)
-       vbox = QVBoxLayout()
-       vbox.addWidget(lcd)
-       vbox.addWidget(sld)
-       self.setLayout(vbox)
-       sld.valueChanged.connect(lcd.display)
-       self.setGeometry(250, 250, 250, 150)
-       self.setWindowTitle('Events demo') 
-       self.show()
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('PyQt events with keyboard') 
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
