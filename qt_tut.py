df522e4909d4bfdf6d9ce09ba9d4ca13f034cb97
Mike <michael.yelverton89@gmail.com>
2022-10-24 19:25:04 -0700

    An example of syncing signals and slots using a QSlider and QLCDNumber object.

--- qt_tut.py
+++ qt_tut.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+
+from PyQt5.QtWidgets import (QApplication, QWidget, QSlider, QLCDNumber, QVBoxLayout)
+from PyQt5.QtCore import Qt
 
 class Example(QWidget):
     def __init__(self):
@@ -7,17 +9,16 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Hello World')
-        self.show()
-
-    def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Quit', 'Are you sure you want to quit?',
-        QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
+       lcd = QLCDNumber(self)
+       sld = QSlider(Qt.Horizontal, self)
+       vbox = QVBoxLayout()
+       vbox.addWidget(lcd)
+       vbox.addWidget(sld)
+       self.setLayout(vbox)
+       sld.valueChanged.connect(lcd.display)
+       self.setGeometry(250, 250, 250, 150)
+       self.setWindowTitle('Events demo') 
+       self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
