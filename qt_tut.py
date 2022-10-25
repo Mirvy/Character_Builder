13eda61bbca64a8c2f09f7f71eb2eda498a22d39
Mike <michael.yelverton89@gmail.com>
2022-10-24 19:37:04 -0700

    Example of positioning widgets by combining layout boxes and using their stretch factor.

--- qt_tut.py
+++ qt_tut.py
@@ -1,27 +1,28 @@
 import sys
-
-from PyQt5.QtWidgets import (QApplication, QWidget, 
-    QToolTip, QPushButton)
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (QApplication, QWidget,
+  QPushButton, QVBoxLayout, QHBoxLayout)
 
 class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-  def __init__(self):
-    super().__init__()
-    self.initUI()
-
-  def initUI(self):
-    QToolTip.setFont(QFont('Arial', 14))
-    self.setToolTip('Tooltip for <b>QWidget</b>')
-    btn = QPushButton('Button', self)
-    btn.setToolTip('Tooltip for <b>QPushButton</b>')
-    btn.resize(btn.sizeHint())
-    btn.move(50, 50)
-    self.setGeometry(300, 300, 300, 220)
-    self.setWindowTitle('PyQt tooltip')    
-    self.show()
+    def initUI(self):
+        okButton = QPushButton('OK')
+        cancelButton = QPushButton('Cancel')
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 350, 150)
+        self.setWindowTitle('Box layout example, QHBoxLayout, QVBoxLayout')  
+        self.show()
 
 if __name__ == '__main__':
-  app = QApplication(sys.argv)
-  ex = Example()
-  sys.exit(app.exec_())
+ app = QApplication(sys.argv)
+ ex = Example()
+ sys.exit(app.exec_())
