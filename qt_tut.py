243aac6717f519d0ae60e355ad558f51c953a4ed
Mike <michael.yelverton89@gmail.com>
2022-10-24 18:58:59 -0700

    Example of QLabel.

--- qt_tut.py
+++ qt_tut.py
@@ -1,34 +1,58 @@
-from PyQt5.QtWidgets import QApplication,QWidget,QTextEdit,QVBoxLayout,QPushButton
 import sys
+from PyQt5.QtWidgets import QVBoxLayout,QMainWindow,QApplication,QLabel,QWidget
+from PyQt5.QtGui import QPixmap, QPalette
+from PyQt5.QtCore import Qt
 
-class TextEditDemo(QWidget):
-    def __init__(self,parent=None):
-        super().__init__(parent)
+class QLabelDemo(QWidget) :
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-        self.setWindowTitle("QTextEdit")
-        self.resize(300,270)
+    def initUI(self):
+        label1 = QLabel(self)
+        label2 = QLabel(self)
+        label3 = QLabel(self)
+        label4 = QLabel(self)
 
-        self.textEdit = QTextEdit()
-        self.btnPress1 = QPushButton("Button 1")
-        self.btnPress2 = QPushButton("Button 2")
+        label1.setText("<font color=yellow>PythonPyQt.com</font>")
+        label1.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Window,Qt.blue)
+        label1.setPalette(palette)
+        label1.setAlignment(Qt.AlignCenter)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.textEdit)
-        layout.addWidget(self.btnPress1)
-        layout.addWidget(self.btnPress2)
-        self.setLayout(layout)
+        label2.setText("<a href='#'>QLabel2</a>")
 
-        self.btnPress1.clicked.connect(self.btnPress1_Clicked)
-        self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+        label3.setAlignment(Qt.AlignCenter)
+        label3.setToolTip('Hint')
+        label3.setPixmap(QPixmap("python.png"))
 
-    def btnPress1_Clicked(self):
-        self.textEdit.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
+        label4.setOpenExternalLinks(True)
+        label4.setText("<a href='https://python.org'>Python.org</a>")
+        label4.setAlignment(Qt.AlignCenter)
+        label4.setToolTip('Python.org')
 
-    def btnPress2_Clicked(self):
-        self.textEdit.setHtml("<font color='red' size='6'><red>Hello PyQt5!\nHello</font>")
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(label1)
+        vbox.addWidget(label2)
+        vbox.addWidget(label3)
+        vbox.addWidget(label4)
+
+        label2.linkHovered.connect(self.linkHovered)
+        label4.linkActivated.connect(self.linkClicked)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QLabel Example')
+
+    def linkHovered(self):
+        print('Link hovered')
+
+    def linkClicked(self):
+        print('Link clicked')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = TextEditDemo()
-    win.show()
+    main = QLabelDemo()
+    main.show()
     sys.exit(app.exec_())
