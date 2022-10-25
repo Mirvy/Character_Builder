6bec950eeaeced226c4e81122366472ab2088add
Mike <michael.yelverton89@gmail.com>
2022-10-25 14:01:09 -0700

    Examples of using different CSS to style progress bars.

--- qt_tut.py
+++ qt_tut.py
@@ -1,50 +1,85 @@
+from random import randint
 import sys
-import time
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QApplication
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QProgressBar
 
-class Thread(QThread):
-    _signal = pyqtSignal(int)
-    def __init__(self):
-        super(Thread, self).__init__()
+StyleSheet = '''
+#RedProgressBar {
+    text-align: center;
+}
+#RedProgressBar::chunk {
+    background-color: #F44336;
+}
+#GreenProgressBar {
+    min-height: 12px;
+    max-height: 12px;
+    border-radius: 6px;
+}
+#GreenProgressBar::chunk {
+    border-radius: 6px;
+    background-color: #009688;
+}
+#BlueProgressBar {
+    border: 2px solid #2196F3;
+    border-radius: 5px;
+    background-color: #E0E0E0;
+}
+#BlueProgressBar::chunk {
+    background-color: #2196F3;
+    width: 10px; 
+    margin: 0.5px;
+}
+'''
 
-    def __del__(self):
-        self.wait()
 
-    def run(self):
-        for i in range(100):
-            time.sleep(0.1)
-            self._signal.emit(i)
+class ProgressBar(QProgressBar):
 
-class Example(QWidget):
-    def __init__(self):
-        super(Example, self).__init__()
-        self.setWindowTitle('QProgressBar')
-        self.btn = QPushButton('Click me')
-        self.btn.clicked.connect(self.btnFunc)
-        self.pbar = QProgressBar(self)
-        self.pbar.setValue(0)
-        self.resize(300, 100)
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.pbar)
-        self.vbox.addWidget(self.btn)
-        self.setLayout(self.vbox)
-        self.show()
+    def __init__(self, *args, **kwargs):
+        super(ProgressBar, self).__init__(*args, **kwargs)
+        self.setValue(0)
+        if self.minimum() != self.maximum():
+            self.timer = QTimer(self, timeout=self.onTimeout)
+            self.timer.start(randint(1, 3) * 1000)
 
-    def btnFunc(self):
-        self.thread = Thread()
-        self.thread._signal.connect(self.signal_accept)
-        self.thread.start()
-        self.btn.setEnabled(False)
+    def onTimeout(self):
+        if self.value() >= 100:
+            self.timer.stop()
+            self.timer.deleteLater()
+            del self.timer
+            return
+        self.setValue(self.value() + 1)
 
-    def signal_accept(self, msg):
-        self.pbar.setValue(int(msg))
-        if self.pbar.value() == 99:
-            self.pbar.setValue(0)
-            self.btn.setEnabled(True)
+
+class Window(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+        self.resize(800, 600)
+        layout = QVBoxLayout(self)
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=100, objectName="RedProgressBar"))
+
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=0, objectName="RedProgressBar"))
+
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=100, textVisible=False,
+                        objectName="GreenProgressBar"))
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=0, textVisible=False,
+                        objectName="GreenProgressBar"))
+
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=100, textVisible=False,
+                        objectName="BlueProgressBar"))
+        layout.addWidget(  
+            ProgressBar(self, minimum=0, maximum=0, textVisible=False,
+                        objectName="BlueProgressBar"))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
+    app.setStyleSheet(StyleSheet)
+    w = Window()
+    w.show()
     sys.exit(app.exec_())
