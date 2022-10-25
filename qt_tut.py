191b80c4475d1f7fb79738c12f6741c30ffc8a86
Mike <michael.yelverton89@gmail.com>
2022-10-24 19:41:15 -0700

    Example of using QProgressBar.

--- qt_tut.py
+++ qt_tut.py
@@ -1,36 +1,50 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget,
-QPushButton, QGridLayout)
+import time
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QApplication
+
+class Thread(QThread):
+    _signal = pyqtSignal(int)
+    def __init__(self):
+        super(Thread, self).__init__()
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        for i in range(100):
+            time.sleep(0.1)
+            self._signal.emit(i)
 
 class Example(QWidget):
-
     def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):   
-        grid = QGridLayout()  
-        self.setLayout(grid)
-
-        names = ['Cls', 'Bck', '', 'Close', 
-        '7', '8', '9', '/',
-        '4', '5', '6', '*',
-        '1', '2', '3', '-',
-        '0', '.', '=', '+',]
-
-        positions = [(i, j) for i in range(5) for j in range(4)]
-
-        for position, name in zip(positions, names):
-         if name == '':
-          continue
-         button = QPushButton(name)
-         grid.addWidget(button, *position)
-
-        self.move(300, 150)
-        self.setWindowTitle('PyQt window')  
+        super(Example, self).__init__()
+        self.setWindowTitle('QProgressBar')
+        self.btn = QPushButton('Click me')
+        self.btn.clicked.connect(self.btnFunc)
+        self.pbar = QProgressBar(self)
+        self.pbar.setValue(0)
+        self.resize(300, 100)
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.pbar)
+        self.vbox.addWidget(self.btn)
+        self.setLayout(self.vbox)
         self.show()
 
-if __name__ == '__main__':
-     app = QApplication(sys.argv)
-     ex = Example()
-     sys.exit(app.exec_()) 
+    def btnFunc(self):
+        self.thread = Thread()
+        self.thread._signal.connect(self.signal_accept)
+        self.thread.start()
+        self.btn.setEnabled(False)
+
+    def signal_accept(self, msg):
+        self.pbar.setValue(int(msg))
+        if self.pbar.value() == 99:
+            self.pbar.setValue(0)
+            self.btn.setEnabled(True)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
