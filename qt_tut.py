30c17e1be85214bb540cb2364fe819fe8f929975
Mike <michael.yelverton89@gmail.com>
2022-10-24 19:39:24 -0700

    An example of using the QGridLaout.

--- qt_tut.py
+++ qt_tut.py
@@ -1,28 +1,36 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget,
-  QPushButton, QVBoxLayout, QHBoxLayout)
+QPushButton, QGridLayout)
 
 class Example(QWidget):
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
-    def initUI(self):
-        okButton = QPushButton('OK')
-        cancelButton = QPushButton('Cancel')
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
-        self.setGeometry(300, 300, 350, 150)
-        self.setWindowTitle('Box layout example, QHBoxLayout, QVBoxLayout')  
+    def initUI(self):   
+        grid = QGridLayout()  
+        self.setLayout(grid)
+
+        names = ['Cls', 'Bck', '', 'Close', 
+        '7', '8', '9', '/',
+        '4', '5', '6', '*',
+        '1', '2', '3', '-',
+        '0', '.', '=', '+',]
+
+        positions = [(i, j) for i in range(5) for j in range(4)]
+
+        for position, name in zip(positions, names):
+         if name == '':
+          continue
+         button = QPushButton(name)
+         grid.addWidget(button, *position)
+
+        self.move(300, 150)
+        self.setWindowTitle('PyQt window')  
         self.show()
 
 if __name__ == '__main__':
- app = QApplication(sys.argv)
- ex = Example()
- sys.exit(app.exec_())
+     app = QApplication(sys.argv)
+     ex = Example()
+     sys.exit(app.exec_()) 
